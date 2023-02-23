2d808d50a9ac09fc6937e8dafa81edbd025b02bc
fgorordo <flgorordo@gmail.com>
2023-02-23 20:07:13 -0300

    project base files, base routes and dependencies

---
+++ src/auth/pages/RegisterPage.jsx
@@ -0,0 +1,7 @@
+export const RegisterPage = () => {
+  return (
+    <>
+      <h2>RegisterPage</h2>
+    </>
+  );
+};
---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,7 @@
+export const JournalPage = () => {
+  return (
+    <>
+      <h2>JournalPage</h2>
+    </>
+  );
+};
